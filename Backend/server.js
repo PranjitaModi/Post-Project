8f7be861d3512bf6922a8b0bef63418a3e4a4b59
PranjitaModi <modipranjita@gmail.com>
2026-06-15 14:08:20 +0530

    Updated project files and folders

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ connectDB();
 
 
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
 })
